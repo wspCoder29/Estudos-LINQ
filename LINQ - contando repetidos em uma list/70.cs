f2aad7131a3c23fce149c6d4613944d17d7c35bf
LeandroFerreira <leandro.wsp@gmail.com>
2022-02-22 16:16:12 -0300

    Adicionado teste contagem de repetidos em uma list

---
+++ LINQ - contando repetidos em uma list/70.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test70
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int [] x = { 2,2,2,2,2,2,2,3,2,2 };
+
+            List<int> li = new List<int>();
+
+            li = x.ToList();
+
+            int qtd = 0;
+
+            qtd = li.Where(x=> x <=2).Count();
+            
+
+            Console.WriteLine(qtd);
+
+            Console.WriteLine(string.Join(" ", li));
+
+
+
+
+
+        }
+    }
+}
