ea40fe994db107d1824f4fcfcd395a4f58a7caf9
LeandroFerreira <leandro.wsp@gmail.com>
2022-02-22 14:43:13 -0300

    Testes com LINQ

---
+++ ExercLinqTest/ExercLinqTest/Pessoa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace ExercLinqTest
+{
+
+    class Pessoa
+    {
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Genero { get; set; }
+
+        public double Salario { get; set; }
+
+        public Pessoa()
+        {
+
+        }
+
+        public Pessoa(string nome, int idade, string genero, double salario)
+        {
+            Nome = nome;
+            Idade = idade;
+            Genero = genero;
+            Salario = salario;
+        }
+
+        public override string ToString()
+        {
+            string str = "\n";
+            str += "Nome: " + this.Nome + Environment.NewLine;
+            str += "Idade: " + this.Idade + Environment.NewLine;
+            str += "Genero: " + this.Genero + Environment.NewLine;
+            str += "Salario R$: " + this.Salario + Environment.NewLine;
+            return str;
+        }
+
+
+        
+
+
+
+
+
+    }
+}
---
+++ ExercLinqTest/ExercLinqTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExercLinqTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa p1 = new Pessoa("Maria", 22, "F", 2000);
+            Pessoa p2 = new Pessoa("Pedro", 18, "M", 1500);
+            Pessoa p3 = new Pessoa("Aya", 18, "F", 900);
+            Pessoa p4 = new Pessoa("Lua", 33, "F", 5000);
+            Pessoa p5 = new Pessoa("Aline", 17, "F", 1700);
+            Pessoa p6 = new Pessoa("Ju", 37, "F", 1000);
+
+            // maior e menor idade
+            // qtd mulheres
+            // Média dos salarios
+            // Gen e idade da pessoa com menor salário
+
+            List<Pessoa> li = new List<Pessoa>();
+            li.Add(p1);
+            li.Add(p2);
+            li.Add(p3);
+            li.Add(p4);
+            li.Add(p5);
+            li.Add(p6);
+
+            Console.WriteLine(String.Join(" ", li));
+            Console.WriteLine("=====================");
+
+            //Ordenando por Idade (menor para maior)
+            li = li.OrderBy(x => x.Idade).ToList();
+
+            //qtd mulheres na região
+            int qtdF = li.Where(x => x.Genero.Equals("F")).Count();
+
+            double mediaSal = li.Average(x => x.Salario);
+
+            Console.WriteLine("Menor idade: "+li[0].Nome+" "+li[0].Idade);
+            Console.WriteLine("Maior idade: " +li[li.Count-1].Nome + " " + li[li.Count-1].Idade);
+            Console.WriteLine("Quantidade de mulheres na região: "+qtdF);
+            Console.WriteLine("Média de salários na região R$:{0:F2}",mediaSal);
+
+
+
+
+
+
+
+        }
+    }
+}
