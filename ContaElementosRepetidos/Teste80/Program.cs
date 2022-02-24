b3a6d1357f11d06c3c17c4feb6a126e8d172e496
LeandroFerreira <leandro.wsp@gmail.com>
2022-02-23 22:41:27 -0300

    VerificaItensRepetidos

---
+++ ContaElementosRepetidos/Teste80/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+
+
+
+namespace Teste80
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dentro de uma lista o seguinte programa organiza os elementos e
+            //Indica quantas vezes os mesmos se repetem
+
+            int[] x = { 2, 5, 4, 3, 6, 2, 5,99, 8, 4, 2, 5, 3 };
+
+            int[] y = x.Distinct().OrderBy(x => x).ToArray();
+
+            Array.Sort(x);
+
+            Console.WriteLine(String.Join(" ", x));
+            Console.WriteLine(String.Join(" ", y));
+
+            int count = 0;
+            Console.WriteLine();
+            for (int i = 0; i< y.Length;i++)
+            {
+                count = x.Where(x => x == y[i]).Count();
+                Console.WriteLine(y[i] + " aparece " + count+"x ");
+            }
+
+            
+        }
+    }
+}
