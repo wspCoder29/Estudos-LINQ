566160924e8a7ba9b796e55b17fec0a2777731e0
LeandroFerreira <leandro.wsp@gmail.com>
2022-03-02 14:43:34 -0300

    Remover repeditos add

---
+++ Removendo repetidos/Removendo Repetidos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Test81
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //REMOVENDO ELEMENTOS REPETIDOS DE ARRAYS
+
+            string str = "3 1 56 77 9 2 1";
+
+            string[] names = { "hello", "hello", "stack", "stack", "overflow", "overflow" };
+
+           var query = names.Distinct();
+
+            Console.WriteLine(string.Join(" ",query));
+
+            int [] x = Array.ConvertAll(str.Split(" "), int.Parse);
+
+            Array.Sort(x);
+
+            var x2 = x.Distinct();
+
+            Console.WriteLine(string.Join(" ", x2));
+
+
+
+        }
+    }
+}
